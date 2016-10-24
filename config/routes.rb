8ced54375004b4ac180bde7e3c0ffa7668588b43
rtomchek <rtomchek@chicagobooth.edu>
2016-10-24 14:52:34 +0000

    RPS_RCAV_HW

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  # Default users to play rock
+  get("/", { :controller => "game", :action => "user_plays_rock"})
+
+  # Select action based on user input
+  get("/rock", { :controller => "game", :action => "user_plays_rock"})
+
+  get("/paper", { :controller => "game", :action => "user_plays_paper"})
+
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors"})
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
